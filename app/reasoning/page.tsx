4c784f329126e23a16952234850f065609725ef3
Alyssa Ooi <z5476339@ad.unsw.edu.au>
2026-03-15 16:47:43 +1100

    feat(reasoning UI): created a header with a png logo

--- app/reasoning/page.tsx
+++ app/reasoning/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const ReasoningPage = styled.div`
   width: 100%;
@@ -13,12 +13,34 @@ const ReasoningPage = styled.div`
 `;
 
 
+const Header = styled.div`
+  width: 100%;
+  height: 120px;
+  text-align: center;
+  background-color: #ffffff;
+  border-bottom: 3.5px solid #253d2c;
+  box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.3);
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default function Home() {
   
 
   return (
     <ReasoningPage>
-      
+      <Header>
+        <Image
+          src="/medichainLogo.png"
+          alt="Medichain logo"
+          width={200}
+          height={200}
+          style={{ position: "absolute", marginRight: "80%" }}
+        />
+      </Header>
     </ReasoningPage>
   );
 }
