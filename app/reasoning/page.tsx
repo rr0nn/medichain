255c895519f2559da028a58549a5525148ff9138
Alyssa Ooi <z5476339@ad.unsw.edu.au>
2026-03-15 16:38:34 +1100

    feat(reasoning UI): installed react-redux and styled components and created empty reasoning page

---
+++ app/reasoning/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styled, { keyframes } from "styled-components";
+
+const ReasoningPage = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  text-align: center;
+  background-color: #f2fff5;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+
+export default function Home() {
+  
+
+  return (
+    <ReasoningPage>
+      
+    </ReasoningPage>
+  );
+}
