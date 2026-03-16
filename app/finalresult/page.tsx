14748e3dbe21cfb2814e2ed9ce9d8a8250667600
Alyssa Ooi <z5476339@ad.unsw.edu.au>
2026-03-16 18:04:36 +1100

    feat(resultUI): added confidence display and guauge bar and adjusted ranking display formatting

--- app/finalresult/page.tsx
+++ app/finalresult/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import styled from "styled-components";
 import Link from 'next/link'
@@ -46,9 +48,38 @@ const DifferentialDiagnosis = styled.div`
 const LinkHome = styled(Link)`
   text-decoration: underline;
   position: absolute;
-  margin-left: 80%
-
+  margin-left: 80%;
+  color: #1B7D7E;
+  font-size: 30px;
+  `
+const BoxFormatingOverall = styled.div`
+  display: flex;
+  flex-direction: row;
 `
+
+const BoxFormatingColumn= styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  padding-left: 10px;
+`
+const ConfidenceFormating = styled.div`
+  display: flex;
+  flex-direction: column;
+  mergin-right: 80%;
+  width: 50%;
+  text-align: right;
+  padding-right: 25px;
+  color: #2e3f50;
+  font-weight: 550;
+`
+const ConfidenceScoreFormating = styled.div`
+  padding-top: 10px;
+  text-align: right;
+  color: #000000;
+  font-weight: bold;
+`
+
 const BoxTitle = styled.div`
   color: #1B7D7E;
   font-size: 25px;
@@ -64,7 +95,6 @@ const ResultDisplay = styled.div`
   font-size: 20px;
   margin-top: 30px;
   width: 94%;
-  height: 150px;
   text-align: left;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
   border-radius: 25px;
@@ -72,7 +102,44 @@ const ResultDisplay = styled.div`
 `
 
 const DiseaseName = styled.div`
-  color: #000000
+  color: #000000;
+  font-size: 17px;
+`
+
+const DxResult = styled.div`
+  color: #000000;
+  font-weight: normal;
+  font-size: 17px;
+`
+const BarBackground = styled.div`
+  margin-top: 10px;
+  width: 98%;
+  background: #c2d8da;
+  border-radius: 20px;
+  height: 24px;
+  overflow: hidden;
+`;
+
+const BarFill = styled.div<{ widthPercent: number}>`
+  width: ${(props) => props.widthPercent}%;
+  background-image: linear-gradient(to right, #5fa7ff, #7bfee2); 
+  height: 100%;
+`;
+
+const TraceMatch = styled.div`
+  margin-top: 15px;
+  color: #000000;
+  font-weight: normal;
+  font-size: 17px;
+  border-radius: 40px;
+  border: 3px solid #bdcdce;
+  display: inline-block;
+  padding-left: 10px;
+  padding-right: 10px;
+`
+
+const FillerDiv = styled.div`
+  margin-top: 10px;
 `
 
 export default function Home() {
@@ -96,16 +163,57 @@ export default function Home() {
           Ranked Output For Differential Diagnosis
         </BoxTitle>
         <ResultDisplay>
-          Rank 1
-          <DiseaseName>
-            Gastric volvulus
-          </DiseaseName>
+          <BoxFormatingOverall>
+            <BoxFormatingColumn>
+              Rank 1
+              <DiseaseName>
+                Gastric volvulus
+              </DiseaseName>
+              <DxResult>
+                dx:gastric_volvulus
+              </DxResult>
+            </BoxFormatingColumn>
+            <ConfidenceFormating>
+              Confidence
+              <ConfidenceScoreFormating>
+                92%
+              </ConfidenceScoreFormating>
+            </ConfidenceFormating>
+          </BoxFormatingOverall>
+          
+          <BarBackground>
+            <BarFill widthPercent={92}/>
+          </BarBackground>
+          <TraceMatch>something </TraceMatch>
+          <FillerDiv></FillerDiv>
         </ResultDisplay>
         <ResultDisplay>
           Rank 2
+          <DiseaseName>
+            Gastric volvulus
+          </DiseaseName>
+          <DxResult>
+            dx:gastric_volvulus
+          </DxResult>
+          <BarBackground>
+            <BarFill widthPercent={68}/>
+          </BarBackground>
+          <TraceMatch>something </TraceMatch>
+          <FillerDiv></FillerDiv>
         </ResultDisplay>
         <ResultDisplay>
           Rank 3
+          <DiseaseName>
+            Gastric volvulus
+          </DiseaseName>
+          <DxResult>
+            dx:gastric_volvulus
+          </DxResult>
+          <BarBackground>
+            <BarFill widthPercent={52}/>
+          </BarBackground>
+          <TraceMatch>something </TraceMatch>
+          <FillerDiv></FillerDiv>
         </ResultDisplay>
       </DifferentialDiagnosis>
     </ResultPage>
